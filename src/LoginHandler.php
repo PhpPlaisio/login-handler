3a93c30f21eb1623427d04cd4fce69c1093c34e1
Paul Water <p.r.water@setbased.nl>
2017-11-23 12:49:48 +0100

    Initial commit.

---
+++ src/LoginHandler.php
@@ -0,0 +1,32 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\LoginProvider;
+
+/**
+ * Interface for login handlers.
+ */
+interface LoginHandler
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of the login response.
+   *
+   * @return int
+   */
+  public function getLgrId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Validates whether the user agent is allowed to login. Returns true is the user agent has successfully logged in.
+   * Returns false otherwise.
+   *
+   * @param array $data The data for validating the credentials.
+   *
+   * @return bool
+   */
+  public function validate(&$data);
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
